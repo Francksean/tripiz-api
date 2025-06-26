67776ffb69d948c057bd846966300e4a57789e2b
Ayza987 <denise.ndongo@2027.ucac-icam.com>
2025-06-26 13:03:50 +0100

    code completion

--- src/main/java/com/tripiz/api/configurations/CorsConfig.java
+++ src/main/java/com/tripiz/api/configurations/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+                //.allowCredentials(true)
                 .maxAge(3600);
     }
 }
