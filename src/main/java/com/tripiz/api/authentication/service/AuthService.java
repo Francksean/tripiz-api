989eb69c5ae544169ada39ec77f0d0e929b0bacb
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2026-07-02 22:16:17 +0100

    update: remove cors origin control

--- src/main/java/com/tripiz/api/authentication/service/AuthService.java
+++ src/main/java/com/tripiz/api/authentication/service/AuthService.java
@@ -51,7 +51,7 @@ public class AuthService {
                 .email(request.getEmail())
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
-                .phone(request.getPhone() != null ? Integer.valueOf(request.getPhone()) : null)
+                .phone(request.getPhone())
                 .role("client")
                 .status("ONLINE")
                 .createdAt(LocalDateTime.now())
