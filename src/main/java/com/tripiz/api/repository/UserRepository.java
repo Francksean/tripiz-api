62ffe3d48fd4f96ba3eb48b41a2607d2c606d396
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2026-07-02 22:34:06 +0100

    update: add some routes

--- src/main/java/com/tripiz/api/repository/UserRepository.java
+++ src/main/java/com/tripiz/api/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.tripiz.api.repository;
 
 import com.tripiz.api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -16,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     long countByCreatedAtAfter(LocalDateTime date);
     Optional<User> findByKeycloakId(String keycloakId);
     List<User> findByRole(String role);
+    @Query("SELECT u.userId FROM User u WHERE u.role = 'driver'")
+    List<UUID> findAllDriverIds();
 }
