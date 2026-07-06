e1dc264e922962e67214f8202a027eb674dfd289
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2026-07-06 09:33:48 +0100

    update

--- src/main/java/com/tripiz/api/authentication/service/KeycloakUserService.java
+++ src/main/java/com/tripiz/api/authentication/service/KeycloakUserService.java
@@ -33,8 +33,8 @@ public class KeycloakUserService {
         try {
             Keycloak keycloak = KeycloakBuilder.builder()
                     .serverUrl(authServerUrl)
-                    .realm("master")
-                    .clientId(adminClientId)        // "admin-cli"
+                    .realm(realm)
+                    .clientId(adminClientId)
                     .clientSecret(adminClientSecret) // injection via @Value
                     .grantType("client_credentials")
                     .build();
