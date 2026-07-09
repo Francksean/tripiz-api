1f7287efae271fa76398d2e364f9adeeb1a50eb0
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2026-07-09 10:43:47 +0100

    update

--- src/main/java/com/tripiz/api/model/ItineraryAdminDTO.java
+++ src/main/java/com/tripiz/api/model/ItineraryAdminDTO.java
@@ -5,12 +5,12 @@ import java.util.UUID;
 
 @Data
 public class ItineraryAdminDTO {
-    private UUID itineraryId;
-    private String routeName;
+    private UUID itinerary_id;
+    private String route_name;
     private String direction;
-    private String itineraryName;
-    private int estimatedDuration;
-    private UUID departureStation;   // ID de la station de départ
-    private UUID arrivalStation;     // ID de la station d'arrivée
+    private String itinerary_name;
+    private int estimated_duration;
+    private UUID departure_station;   // ID de la station de départ
+    private UUID arrival_station;     // ID de la station d'arrivée
     private double distance;
 }
