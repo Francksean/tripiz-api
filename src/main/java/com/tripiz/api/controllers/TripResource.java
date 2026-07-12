b3505fa45e32bb2edf1819cdbc44b4feedb9e321
Ayza987 <denise.ndongo@2027.ucac-icam.com>
2026-07-12 16:25:49 +0100

    Code completion

--- src/main/java/com/tripiz/api/controllers/TripResource.java
+++ src/main/java/com/tripiz/api/controllers/TripResource.java
@@ -91,7 +91,6 @@ public class TripResource {
     }
 
     @GetMapping("/getByStation/{stationId}")
-    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<List<TripWithItineraryDetailsDTO>> getTripsByStation(
             @PathVariable UUID stationId) {
 
